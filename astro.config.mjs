968950ae8ff103a9d44f92307562ea3d99bd35ab
koyaman <koyaman@concentinc.jp>
2023-02-15 00:27:55 +0900

    fix astro.config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 
 export default defineConfig({
-	site: 'https://8951172c.astronotion.pages.dev/',
+	site: 'https://astronotion.pages.dev/',
 	// base: '/astronotion',
 	integrations: [mdx(), sitemap()],
 });
