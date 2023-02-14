3b0a85a781052c213a385be8802718b818e0aebe
koyaman <koyaman@concentinc.jp>
2023-02-14 18:15:02 +0900

    change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 
 export default defineConfig({
-	// site: 'https://koyaman.github.io',
-	// base: '',
+	site: 'https://8951172c.astronotion.pages.dev/',
+	// base: '/astronotion',
 	integrations: [mdx(), sitemap()],
 });
